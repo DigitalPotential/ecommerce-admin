9421f4650def6974e46e24292c69022ff1e8409f
DigitalPotential <Carlos@digitalpotential.se>
2023-08-03 17:52:03 +0200

    feat: formSchema with zod

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,18 +1,39 @@
 "use client";
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal()
+    const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        //todo create store
+        console.log(values);
+    }
 
     return (
-    <Modal
-        title="Create store" 
-        description="Add a new store to manage products and categories" 
-        isOpen={storeModal.isOpen} 
-        onClose={storeModal.onClose}>
-        Future Create Store Form
-    </Modal>
-    )
-}
+        <Modal
+            title="Create store"
+            description="Add a new store to manage products and categories"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            Future Create Store Form
+        </Modal>
+    );
+};
