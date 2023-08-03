ad6e37ba55f30afd4cb149f7a47a9e7a2305c929
DigitalPotential <Carlos@digitalpotential.se>
2023-08-03 21:20:01 +0200

    feat: prisma configured, create stores api

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,13 +2,22 @@
 
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios"
 
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { Modal } from "../ui/modal";
-import { useForm } from "react-hook-form";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Modal } from "@/components/ui/modal";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -16,6 +25,8 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -25,9 +36,18 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        //todo create store
-        console.log(values);
-    }
+        try {
+            setLoading(true);
+
+            const response = await axios.post("/api/stores", values);
+
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <Modal
@@ -47,15 +67,25 @@ export const StoreModal = () => {
                                     <FormItem>
                                         <FormLabel>Name</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="E-Commerce" {...field} />
+                                            <Input
+                                                disabled={loading}
+                                                placeholder="E-Commerce"
+                                                {...field}
+                                            />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
-                                )}  
+                                )}
                             />
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                                <Button type="submit">Continue</Button>
+                                <Button
+                                    disabled={loading}
+                                    variant="outline"
+                                    onClick={storeModal.onClose}
+                                >
+                                    Cancel
+                                </Button>
+                                <Button disabled={loading} type="submit">Continue</Button>
                             </div>
                         </form>
                     </Form>
