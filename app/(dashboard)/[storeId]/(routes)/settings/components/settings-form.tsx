e8bf23debd28018140b9f1e18d0f826d842871d4
DigitalPotential <Carlos@digitalpotential.se>
2023-08-04 20:16:30 +0200

    feat: settingsform part 2

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,23 +1,48 @@
-"use client"
+"use client";
 
-import { Store } from "@prisma/client"
-import { Heading } from "@/components/ui/heading"
+import * as z from "zod";
+import { Store } from "@prisma/client";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 interface SettingsFormProps {
-    initialData: Store
+    initialData: Store;
 }
 
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
-export const SettingsForm: React.FC<SettingsFormProps> = ({
-    initialData
-}) => {
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    })
+
     return (
-    <div className="flex items-center justify-between">
-        <Heading
-            title="Settings"
-            description="Manage store preferences."
-        />
-    </div>
-    )
-}
-
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title="Settings"
+                    description="Manage store preferences."
+                />
+                <Button variant="destructive" size="icon" onClick={() => {}}>
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+        </>
+    );
+};
