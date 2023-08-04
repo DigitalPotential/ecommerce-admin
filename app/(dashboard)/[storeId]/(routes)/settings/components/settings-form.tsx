aaced77e93d674bd57901a75fa1fb0148feb6bef
DigitalPotential <Carlos@digitalpotential.se>
2023-08-04 22:10:48 +0200

    feat: API calls triggered by UI

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -9,8 +9,18 @@ import { Separator } from "@/components/ui/separator";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -22,19 +32,30 @@ const formSchema = z.object({
 
 type SettingsFormValues = z.infer<typeof formSchema>;
 
-
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
-    const [open, setOpen] = useState(false)
-    const [loading, setLoading] = useState(false)
+    const params = useParams();
+    const router = useRouter()
+
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<SettingsFormValues>({
         resolver: zodResolver(formSchema),
-        defaultValues: initialData
-    })
+        defaultValues: initialData,
+    });
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data)
-    }
+        try {
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh()
+            toast.success("Store updated.");
+        } catch (error) {
+            toast.error("Something went wrong.");
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <>
@@ -43,29 +64,45 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
                     title="Settings"
                     description="Manage store preferences."
                 />
-                <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
+                <Button
+                    disabled={loading}
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => setOpen(true)}
+                >
                     <Trash className="h-4 w-4" />
                 </Button>
             </div>
             <Separator />
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8 w-full"
+                >
                     <div className="grid grid-cols-3 gap-8">
-                        <FormField 
+                        <FormField
                             control={form.control}
                             name="name"
                             render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Name</FormLabel>
                                     <FormControl>
-                                        <Input disabled={loading} placeholder="Store name" {...field} />
+                                        <Input
+                                            disabled={loading}
+                                            placeholder="Store name"
+                                            {...field}
+                                        />
                                     </FormControl>
-                                    <FormMessage  />
+                                    <FormMessage />
                                 </FormItem>
                             )}
                         />
                     </div>
-                    <Button disabled={loading} className="ml-auto" type="submit">
+                    <Button
+                        disabled={loading}
+                        className="ml-auto"
+                        type="submit"
+                    >
                         Save changes
                     </Button>
                 </form>
