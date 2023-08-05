100be9771197dcf38c1e10e56fe7e615a5b38bc7
DigitalPotential <Carlos@digitalpotential.se>
2023-08-05 21:15:22 +0200

    feat: typo

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -79,7 +79,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
             <AlertModal
                 isOpen={open}
                 onClose={() => setOpen(false)}
-                onConfirm={() => {onDelete}}
+                onConfirm={onDelete}
                 loading={loading}
             />
             <div className="flex items-center justify-between">
