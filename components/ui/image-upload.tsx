8ab13299092e95c8df251963c1028ca7277b5441
DigitalPotential <Carlos@digitalpotential.se>
2023-08-08 23:34:47 +0200

    feat: ImageUpload feature started

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+    disabled?: boolean;
+    onChange: (value: string) => void;
+    onRemove: (value: string) => void;
+    value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+    disabled,
+    onChange,
+    onRemove,
+    value
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+
+    return (
+        <div>
+            <h1>Image Upload</h1>
+        </div>
+    )
+}
+
+export default ImageUpload;
