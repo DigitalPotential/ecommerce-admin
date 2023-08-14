7ed15508757df7c739947217aa73552a9dda1fd6
DigitalPotential <Carlos@digitalpotential.se>
2023-08-14 18:49:43 +0200

    feat: added action copy update. Delete not done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -5,6 +5,11 @@ import { BillboardColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+import { toast } from "react-hot-toast"
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
     data: BillboardColumn;
@@ -13,7 +18,38 @@ interface CellActionProps {
 export const CellAction: React.FC<CellActionProps> = ({
     data
 }) => {
+    const params = useParams();
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id)
+        toast.success("Billboard Id copied to the clipboard.")
+    }
+
+    const onDelete = async () => {
+        try {
+            setLoading(true);
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+            router.refresh();
+            router.push("/");
+            toast.success("Billboard deleted.");
+        } catch (error) {
+            toast.error(
+                "Make sure you removed all categories using this billboard first."
+            );
+        } finally {
+            setLoading(false);
+            setOpen(false);
+        }
+    };
+
+
     return (
+        <>
+        <AlertModal />
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="h-8 w-8 p-0">
@@ -26,18 +62,19 @@ export const CellAction: React.FC<CellActionProps> = ({
                     Actions
                 </DropdownMenuLabel>
                 <DropdownMenuItem>
-                    <Copy className="mr-2 h-4 w-4" />
+                    <Copy onClick={() => onCopy(data.id)} className="mr-2 h-4 w-4" />
                     Copy Id
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
                     <Edit className="mr-2 h-4 w-4" />
                     Update
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={() => }>
                     <Trash className="mr-2 h-4 w-4" />
                     Delete
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
+        </>
     )
 }
