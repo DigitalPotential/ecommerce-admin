60a178f0e237a975a2ddc57934cc1692187fa9fb
DigitalPotential <Carlos@digitalpotential.se>
2023-08-14 18:37:30 +0200

    feat: Cell Action UI done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,6 +1,43 @@
-export const CellAction = () => {
-    return (
-        <div>Action</div>
-    )
+"use client";
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { BillboardColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+
+interface CellActionProps {
+    data: BillboardColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({
+    data
+}) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>
+                    Actions
+                </DropdownMenuLabel>
+                <DropdownMenuItem>
+                    <Copy className="mr-2 h-4 w-4" />
+                    Copy Id
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Edit className="mr-2 h-4 w-4" />
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Trash className="mr-2 h-4 w-4" />
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 }
