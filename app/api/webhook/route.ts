67112deb827bc3c7ef410a5b5a0d9dbdec8dd036
DigitalPotential <Carlos@digitalpotential.se>
2023-08-28 16:55:34 +0200

    feat: Dashboard Page, Cards

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,6 +5,8 @@ import { NextResponse } from "next/server"
 import { stripe } from "@/lib/stripe"
 import prismadb from "@/lib/prismadb"
 
+//test
+
 export async function POST(req: Request) {
   const body = await req.text()
   const signature = headers().get("Stripe-Signature") as string
